18a8c3ae9628a7a472c43445fb98345b4a63443f
Scratch-Maru <319314809+maru-m4ru-maru@users.noreply.github.com>
2026-09-08 18:25:13 +0900

    Update kernel.asm

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,19 +1,19 @@
-; SafirOS Kernel
-; GPL-3.0-or-later
-
 bits 16
 org 0x0000
 
 start:
 
-    ; 画面をクリア
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+
     mov ax, 0x0003
     int 0x10
 
-    ; カーネル起動メッセージ
     mov si, message
 
 print_loop:
+
     lodsb
 
     cmp al, 0
@@ -26,15 +26,16 @@ print_loop:
     jmp print_loop
 
 halt:
+
     cli
     hlt
+
     jmp halt
 
 message db "SafirOS Kernel v0.1", 13, 10
         db "Kernel loaded successfully.", 13, 10
         db "> ", 0
 
-; 1セクタ(512 bytes)にする
 times 510 - ($ - $$) db 0
 
 dw 0x0000
