dc53b029bd75dedea8d24adcdf86c3fa9ebb7f80
Scratch-Maru <319314809+maru-m4ru-maru@users.noreply.github.com>
2026-09-08 23:33:16 +0900

    Update boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,30 +1,22 @@
-; SafirOS Bootloader
-; GPL-3.0-or-later
-
 bits 16
 org 0x7C00
 
 start:
 
-    ; セグメント設定
     xor ax, ax
     mov ds, ax
     mov es, ax
-
     mov ss, ax
     mov sp, 0x7C00
 
-    ; BIOSから渡された起動ドライブを保存
     mov [boot_drive], dl
 
-    ; カーネルを 0x1000:0000 に読み込む
     mov ax, 0x1000
     mov es, ax
     xor bx, bx
 
-    ; ディスク読み込み
     mov ah, 0x02
-    mov al, 0x01
+    mov al, 0x02
     mov ch, 0x00
     mov cl, 0x02
     mov dh, 0x00
@@ -33,7 +25,6 @@ start:
     int 0x13
     jc disk_error
 
-    ; カーネルへジャンプ
     jmp 0x1000:0x0000
 
 
@@ -70,7 +61,6 @@ error_halt:
 boot_drive db 0
 
 error_message db "SafirOS: Kernel load failed.", 13, 10, 0
-
 
 times 510 - ($ - $$) db 0
 
