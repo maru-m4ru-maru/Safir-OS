060f9e2574e70a15393f3ffa7842e0f69c3d6d6e
Scratch-Maru <code-maru@outlook.jp>
2026-09-21 08:46:51 +0900

    Separate host test panic configuration

--- kernel/rust/src/lib.rs
+++ kernel/rust/src/lib.rs
@@ -3,12 +3,12 @@
 pub mod memory;
 mod vga;
 
-#[cfg(not(test))]
+#[cfg(not(feature = "host-test"))]
 use core::panic::PanicInfo;
 
-#[cfg(not(test))]
 pub use memory::Bitmap;
 
+#[cfg(not(feature = "host-test"))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {
@@ -24,6 +24,7 @@ pub extern "C" fn rust_main() {
 
     let mut writer = vga::Writer::new();
     writer.clear();
-    writer.write_bytes(b"SafirOS Rust Kernel\n");
+    writer.write_bytes(b"SafirOS Rust Kernel
+");
     writer.write_bytes(b"Kernel Core: OK");
 }
