c795c311d41584b098eb31106f4c00054aa8bab5
Scratch-Maru <code-maru@outlook.jp>
2026-09-21 22:05:58 +0900

    Expose physical memory manager

--- kernel/rust/src/memory/mod.rs
+++ kernel/rust/src/memory/mod.rs
@@ -1,7 +1,9 @@
 mod bitmap;
 mod e820;
 mod frame;
+mod pmm;
 
 pub use bitmap::Bitmap;
 pub use e820::{E820Entry, MemoryMap, MemoryRegion};
 pub use frame::{FrameAllocator, PhysFrame, PAGE_SIZE};
+pub use pmm::PhysicalMemoryManager;
