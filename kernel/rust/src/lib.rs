ae45629cec0cf3db567c0414131a8a16083e3470
Scratch-Maru <code-maru@outlook.jp>
2026-09-22 02:39:00 +0900

    Reserve preemption hook and task stacks

--- kernel/rust/src/lib.rs
+++ kernel/rust/src/lib.rs
@@ -45,6 +45,8 @@ pub extern "C" fn rust_main(e820_ptr: u64, e820_len: usize, test_mode: u64) {
 
     physical_memory.reserve_range(0x00006000, 32 * 24);
     physical_memory.reserve_range(0x00010000, 8192);
+    physical_memory.reserve_range(0x0005F000, 0x1000);
+    physical_memory.reserve_range(0x00060000, 0x8000);
     physical_memory.reserve_range(0x00068000, 0x4000);
     physical_memory.reserve_range(0x00080000, 0x1000);
     physical_memory.reserve_range(0x00090000, 0x3000);
