7a5dfeeb7f92b6d4c1877cf29adca4c4477903a9
Scratch-Maru <code-maru@outlook.jp>
2026-09-22 03:28:07 +0900

    Implement preemptive x86-64 interrupt switching

--- kernel/rust/src/context_switch.rs
+++ kernel/rust/src/context_switch.rs
@@ -5,228 +5,180 @@ use crate::CpuContext;
 global_asm!(r#"
 .intel_syntax noprefix
 
-.equ CTX_A, 0x68000
-.equ CTX_B, 0x68080
-.equ TEST_STACK_B_TOP, 0x6B000
-
 .global safiros_context_switch
 .type safiros_context_switch, @function
 safiros_context_switch:
-    mov qword ptr [rdi + 0], r15
-    mov qword ptr [rdi + 8], r14
-    mov qword ptr [rdi + 16], r13
-    mov qword ptr [rdi + 24], r12
-    mov qword ptr [rdi + 32], rbx
-    mov qword ptr [rdi + 40], rbp
-
-    mov rax, qword ptr [rsp]
-    mov qword ptr [rdi + 56], rax
+    mov qword ptr [rdi + 0], rax
+    mov qword ptr [rdi + 8], rbx
+    mov qword ptr [rdi + 16], rcx
+    mov qword ptr [rdi + 24], rdx
+    mov qword ptr [rdi + 32], rsi
+    mov qword ptr [rdi + 40], rdi
+    mov qword ptr [rdi + 48], rbp
+    mov qword ptr [rdi + 56], r8
+    mov qword ptr [rdi + 64], r9
+    mov qword ptr [rdi + 72], r10
+    mov qword ptr [rdi + 80], r11
+    mov qword ptr [rdi + 88], r12
+    mov qword ptr [rdi + 96], r13
+    mov qword ptr [rdi + 104], r14
+    mov qword ptr [rdi + 112], r15
     lea rax, [rsp + 8]
-    mov qword ptr [rdi + 48], rax
-
+    mov qword ptr [rdi + 120], rax
+    mov rax, qword ptr [rsp]
+    mov qword ptr [rdi + 128], rax
     pushfq
     pop rax
-    mov qword ptr [rdi + 64], rax
+    mov qword ptr [rdi + 136], rax
 
-    mov r15, qword ptr [rsi + 0]
-    mov r14, qword ptr [rsi + 8]
-    mov r13, qword ptr [rsi + 16]
-    mov r12, qword ptr [rsi + 24]
-    mov rbx, qword ptr [rsi + 32]
-    mov rbp, qword ptr [rsi + 40]
-
-    mov rax, qword ptr [rsi + 64]
+    mov rax, qword ptr [rsi + 136]
     push rax
     popfq
-
-    mov rsp, qword ptr [rsi + 48]
-    push qword ptr [rsi + 56]
+    mov r15, qword ptr [rsi + 112]
+    mov r14, qword ptr [rsi + 104]
+    mov r13, qword ptr [rsi + 96]
+    mov r12, qword ptr [rsi + 88]
+    mov r11, qword ptr [rsi + 80]
+    mov r10, qword ptr [rsi + 72]
+    mov r9, qword ptr [rsi + 64]
+    mov r8, qword ptr [rsi + 56]
+    mov rbp, qword ptr [rsi + 48]
+    mov rdx, qword ptr [rsi + 24]
+    mov rcx, qword ptr [rsi + 16]
+    mov rbx, qword ptr [rsi + 8]
+    mov rdi, qword ptr [rsi + 40]
+    mov rsi, qword ptr [rsi + 32]
+    mov rsp, qword ptr [rsi + 120]
+    mov rax, qword ptr [rsi + 128]
+    push rax
+    mov rax, qword ptr [rsi + 0]
     ret
 
-.global safiros_context_switch_smoke_test
-.type safiros_context_switch_smoke_test, @function
-safiros_context_switch_smoke_test:
+.global safiros_start_first_task
+.type safiros_start_first_task, @function
+safiros_start_first_task:
+    mov rax, qword ptr [rdi + 136]
+    push rax
+    popfq
+    mov r15, qword ptr [rdi + 112]
+    mov r14, qword ptr [rdi + 104]
+    mov r13, qword ptr [rdi + 96]
+    mov r12, qword ptr [rdi + 88]
+    mov r11, qword ptr [rdi + 80]
+    mov r10, qword ptr [rdi + 72]
+    mov r9, qword ptr [rdi + 64]
+    mov r8, qword ptr [rdi + 56]
+    mov rbp, qword ptr [rdi + 48]
+    mov rdx, qword ptr [rdi + 24]
+    mov rcx, qword ptr [rdi + 16]
+    mov rbx, qword ptr [rdi + 8]
+    mov rdi, qword ptr [rdi + 40]
+    mov rsi, qword ptr [rdi + 32]
+    mov rsp, qword ptr [rdi + 120]
+    sub rsp, 24
+    mov rax, qword ptr [rdi + 128]
+    mov qword ptr [rsp + 0], rax
+    mov qword ptr [rsp + 8], 0x18
+    mov rax, qword ptr [rdi + 136]
+    mov qword ptr [rsp + 16], rax
+    mov rax, qword ptr [rdi + 0]
+    iretq
+
+.global safiros_restore_context
+.type safiros_restore_context, @function
+safiros_restore_context:
+    mov rax, qword ptr [rdi + 136]
+    push rax
+    popfq
+    mov r15, qword ptr [rdi + 112]
+    mov r14, qword ptr [rdi + 104]
+    mov r13, qword ptr [rdi + 96]
+    mov r12, qword ptr [rdi + 88]
+    mov r11, qword ptr [rdi + 80]
+    mov r10, qword ptr [rdi + 72]
+    mov r9, qword ptr [rdi + 64]
+    mov r8, qword ptr [rdi + 56]
+    mov rbp, qword ptr [rdi + 48]
+    mov rdx, qword ptr [rdi + 24]
+    mov rcx, qword ptr [rdi + 16]
+    mov rbx, qword ptr [rdi + 8]
+    mov rsi, qword ptr [rdi + 32]
+    mov rsp, qword ptr [rdi + 120]
+    sub rsp, 24
+    mov rax, qword ptr [rdi + 128]
+    mov qword ptr [rsp + 0], rax
+    mov qword ptr [rsp + 8], 0x18
+    mov rax, qword ptr [rdi + 136]
+    mov qword ptr [rsp + 16], rax
+    mov rdi, qword ptr [rdi + 40]
+    mov rax, qword ptr [rsp]
+    mov rax, qword ptr [rdi + 0]
+    iretq
+
+.global safiros_timer_interrupt
+.type safiros_timer_interrupt, @function
+safiros_timer_interrupt:
+    push r15
+    push r14
+    push r13
+    push r12
+    push r11
+    push r10
+    push r9
+    push r8
+    push rdi
+    push rsi
     push rbp
     push rbx
-    push r12
-    push r13
-    push r14
-    push r15
-    pushfq
-    cli
+    push rdx
+    push rcx
+    push rax
 
-    mov r15, 0x1515151515151515
-    mov r14, 0x1414141414141414
-    mov r13, 0x1313131313131313
-    mov r12, 0x1212121212121212
-    mov rbx, 0xB1B1B1B1B1B1B1B1
-    mov rbp, 0xA1A1A1A1A1A1A1A1
-
-    mov rax, 0x2525252525252525
-    mov qword ptr [CTX_B + 0], rax
-    mov rax, 0x2424242424242424
-    mov qword ptr [CTX_B + 8], rax
-    mov rax, 0x2323232323232323
-    mov qword ptr [CTX_B + 16], rax
-    mov rax, 0x2222222222222222
-    mov qword ptr [CTX_B + 24], rax
-    mov rax, 0xC1C1C1C1C1C1C1C1
-    mov qword ptr [CTX_B + 32], rax
-    mov rax, 0xD1D1D1D1D1D1D1D1
-    mov qword ptr [CTX_B + 40], rax
-    mov qword ptr [CTX_B + 48], TEST_STACK_B_TOP - 8
-
-    lea rax, [rip + context_test_b]
-    mov qword ptr [CTX_B + 56], rax
-    mov qword ptr [CTX_B + 64], 0x2
-
-    mov al, 'C'
-    out 0xE9, al
-
-    mov rdi, CTX_A
-    mov rsi, CTX_B
-    call safiros_context_switch
-
-    mov rax, 0x1515151515151515
-    cmp r15, rax
-    jne context_test_fail
-    mov rax, 0x1414141414141414
-    cmp r14, rax
-    jne context_test_fail
-    mov rax, 0x1313131313131313
-    cmp r13, rax
-    jne context_test_fail
-    mov rax, 0x1212121212121212
-    cmp r12, rax
-    jne context_test_fail
-    mov rax, 0xB1B1B1B1B1B1B1B1
-    cmp rbx, rax
-    jne context_test_fail
-    mov rax, 0xA1A1A1A1A1A1A1A1
-    cmp rbp, rax
-    jne context_test_fail
-
-    mov al, 'X'
-    out 0xE9, al
-
-    mov rdi, CTX_A
-    mov rsi, CTX_B
-    call safiros_context_switch
-
-    mov rax, 0x1515151515151515
-    cmp r15, rax
-    jne context_test_fail
-    mov rax, 0x1414141414141414
-    cmp r14, rax
-    jne context_test_fail
-    mov rax, 0x1313131313131313
-    cmp r13, rax
-    jne context_test_fail
-    mov rax, 0x1212121212121212
-    cmp r12, rax
-    jne context_test_fail
-    mov rax, 0xB1B1B1B1B1B1B1B1
-    cmp rbx, rax
-    jne context_test_fail
-    mov rax, 0xA1A1A1A1A1A1A1A1
-    cmp rbp, rax
-    jne context_test_fail
-
-context_test_done:
-    mov al, 'W'
-    out 0xE9, al
-    popfq
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop rbx
-    pop rbp
-    mov eax, 1
-    ret
-
-context_test_b:
-    pushfq
-    pop rax
-    cmp rax, 0x2
-    jne context_test_fail
-
-    mov rax, rsp
-    cmp rax, TEST_STACK_B_TOP - 8
-    jne context_test_fail
-
-    mov rax, 0x2525252525252525
-    cmp r15, rax
-    jne context_test_fail
-    mov rax, 0x2424242424242424
-    cmp r14, rax
-    jne context_test_fail
-    mov rax, 0x2323232323232323
-    cmp r13, rax
-    jne context_test_fail
-    mov rax, 0x2222222222222222
-    cmp r12, rax
-    jne context_test_fail
-    mov rax, 0xC1C1C1C1C1C1C1C1
-    cmp rbx, rax
-    jne context_test_fail
-    mov rax, 0xD1D1D1D1D1D1D1D1
-    cmp rbp, rax
-    jne context_test_fail
+    mov r12, rsp
+    mov rdi, rsp
 
     mov al, 'T'
     out 0xE9, al
 
-    mov rdi, CTX_B
-    mov rsi, CTX_A
-    call safiros_context_switch
+    mov al, 0x20
+    out 0x20, al
 
-    mov rax, 0x2525252525252525
-    cmp r15, rax
-    jne context_test_fail
-    mov rax, 0x2424242424242424
-    cmp r14, rax
-    jne context_test_fail
-    mov rax, 0x2323232323232323
-    cmp r13, rax
-    jne context_test_fail
-    mov rax, 0x2222222222222222
-    cmp r12, rax
-    jne context_test_fail
-    mov rax, 0xC1C1C1C1C1C1C1C1
-    cmp rbx, rax
-    jne context_test_fail
-    mov rax, 0xD1D1D1D1D1D1D1D1
-    cmp rbp, rax
-    jne context_test_fail
+    and rsp, -16
+    call safiros_timer_schedule
+    mov rdi, rax
+    mov rsp, r12
+    jmp safiros_restore_context
 
-    mov al, 'S'
+.global safiros_task_a
+.type safiros_task_a, @function
+safiros_task_a:
+.task_a_loop:
+    mov al, 'A'
     out 0xE9, al
-
-    mov rdi, CTX_B
-    mov rsi, CTX_A
-    call safiros_context_switch
-
-context_test_fail:
-    mov al, 'F'
-    out 0xE9, al
-    cli
-.halt:
     hlt
-    jmp .halt
+    jmp .task_a_loop
+
+.global safiros_task_b
+.type safiros_task_b, @function
+safiros_task_b:
+.task_b_loop:
+    mov al, 'B'
+    out 0xE9, al
+    hlt
+    jmp .task_b_loop
 
 .att_syntax
 "#);
 
 unsafe extern "C" {
     fn safiros_context_switch(old: *mut CpuContext, new: *const CpuContext);
-    fn safiros_context_switch_smoke_test() -> u64;
+    fn safiros_start_first_task(context: *const CpuContext) -> !;
 }
 
 pub unsafe fn context_switch(old: &mut CpuContext, new: &CpuContext) {
     safiros_context_switch(old as *mut CpuContext, new as *const CpuContext);
 }
 
-pub unsafe fn qemu_smoke_test() -> bool {
-    safiros_context_switch_smoke_test() != 0
+pub unsafe fn start_first_task(context: *const CpuContext) -> ! {
+    safiros_start_first_task(context)
 }
